a9e726d6a681e8d16f0e581303311af1bad30d1c
james23216 <jhmun23216@gmail.com>
2022-04-26 04:55:19 -0700

    update pakcage.json

--- client/src/pages/rentals/search/Search.js
+++ client/src/pages/rentals/search/Search.js
@@ -5,12 +5,21 @@ import {
   Row,
 } from "reactstrap";
 
+import Widget from "../../../components/Widget/Widget";
+
 const Search = () => {
   return (
     <div>
       <Row>
         <Col className="pr-grid-col" xs={12} lg={12}>
-          
+          <Widget className="widget-p-lg">
+            <div className="d-flex align-items-center justify-content-between py-3">
+              dsada
+            </div>
+            <div className="my-2">
+              <p>This is the Progressive Web Application for Real-Eastate Investor</p>
+            </div>
+          </Widget>
         </Col>
       </Row>
     </div>
