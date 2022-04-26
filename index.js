57ff353631b80be616ba820d46435a82f2a4c587
james23216 <jhmun23216@gmail.com>
2022-04-26 05:28:12 -0700

    update server with correct path

--- index.js
+++ index.js
@@ -1,38 +1,22 @@
-// const express = require("express");
-// const path = require('path');
-
-// const PORT = process.env.PORT || 3001;
-
-// const app = express();
-
-
-// // Have Node serve the files for our built React app
-// app.use(express.static(path.resolve(__dirname, '../client/build')));
-
-// // Handle GET requests to /api route
-// app.get("/api", (req, res) => {
-//   res.json({ message: "Hello from server!" });
-// });
-
-// // All other GET requests not handled before will return our React app
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-// });
-
-
-
-//Install express server
-const express = require('express');
+const express = require("express");
 const path = require('path');
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname, '/client/build'));
 
-app.get('/*', function(req,res) {
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
-res.sendFile(path.join(__dirname + '/client/build/index.html'));
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
 
 // Start the app by listening on the default Heroku port
